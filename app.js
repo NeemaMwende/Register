b1aa52be8de0252ffae504fa0072a0370d5ce5f4
Neema_Mwende <gracepete12@gmail.com>
2023-07-17 12:03:10 +0300

    Js

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hey');
+console.log('This is my 1st day Learning Javascript');
+
+let x = 2;
+let y = 8;
+let z = x + y;
+console.log('Answer:' + z);
---
+++ variables.js
@@ -0,0 +1,4 @@
+let x = 2;
+let y = 8;
+let z = x + y;
+console.log('Answer:' + z);
