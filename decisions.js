17da74e72e80a739dd46a695b1412bdbae6e0357
Neema_Mwende <gracepete12@gmail.com>
2023-07-20 15:11:49 +0300

    functions

---
+++ decisions.js
@@ -0,0 +1,38 @@
+// // if statement
+// var count = 5;
+// if (count == 3)
+// {
+//     console.log('count is 3');
+// }else if (count > 4)
+// {
+//     console.log('count is greater than 4');
+// }else if (count < 4)
+// {
+//     console.log('count is less than 4');
+// }
+// else
+// {
+//     console.log('count is not 3');
+// }
+
+//switch statement
+// let hero = 'Were-wolf';
+// switch (hero.toLowerCase()) //to find variables in whatever case either upper or lower
+// {
+//     case 'vampire': console.log('has fangs'); break;
+//     case 'were-wolf': console.log('has fur'); break;
+//     case 'hybrid': console.log('both fangs and fun'); break;
+//     case 'witch': console.log('magic strength'); break;
+//     case 'doppleganger': console.log('look alike'); break;
+//     case 'psychic': console.log('can read minds'); break;
+//     case 'siren': console.log('can sing'); break;
+//     default: console.log('member of mystic falls');break;
+// }
+
+//ternary operator
+let a = 1, b = '1';
+// let result = (a === b) ? 'equal' : 'inequal';
+// let result = (a === b) ? 'equal' : 'inequal';
+let result = (a != b) ? 'not equal' : ' not inequal';
+console.log(result);
+// console.log((a == b) ? 'equal' : 'inequal');
