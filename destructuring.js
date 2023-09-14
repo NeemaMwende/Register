d3164c791c109589f173f982a9d86aa4c9de848b
Neema_Mwende <gracepete12@gmail.com>
2023-09-14 22:00:32 +0300

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+let a,b,c,d,e;
+
+let names = ['car', 'boy', 'march', 'house', 'guest'];
+[a,b,c,d,e] = names;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+// document.write.getElementIdByName();
