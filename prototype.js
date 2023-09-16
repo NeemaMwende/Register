6901cf14138cc8023809a89019e2104d0ccc8e69
Neema_Mwende <gracepete12@gmail.com>
2023-09-16 18:59:54 +0300

    updates

--- prototype.js
+++ prototype.js
@@ -13,4 +13,39 @@ let originalCar =
 // creating a new object from an existing object
 let newCar = Object.create(originalCar);
 console.log(newCar.make);
+// to get a prototype of the new car
+console.log(Object.getPrototypeOf(newCar)); 
+
+// let myPrototype = Object.getPrototypeOf(newCar);
+// console.log(myPrototype.year);
+
+originalCar.doors = 4;
+originalCar.color = 'red';
+console.log(newCar.doors);
+console.log(Object.getPrototypeOf(newCar));
+
+// newCar.cc = 1800;
+// newCar.color = 'blue';
+// // console.log(Object.getPrototypeOf(originalCar));
+// console.log(Object.getPrototypeOf(newCar));
+console.log(newCar.cc);
+
+// to display the attributes of the original car
+for (let key in originalCar) {
+    console.log(key + ": " + originalCar[key]);
+}
+
+
+newCar.cc = 1800;
+newCar.color = 'blue';
+
+// Creating a new object from an existing object
+let newCar = Object.create(originalCar);
+newCar.color = 'Blue';
+
+for (let key in newCar) {
+    console.log(key + ": " + newCar[key]);
+}
+
+
 
