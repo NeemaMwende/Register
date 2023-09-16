eceb9826f99b6e9c4c7f9d138ef7d10978f8f879
Neema_Mwende <gracepete12@gmail.com>
2023-09-16 18:07:31 +0300

    constructor

---
+++ constructor.js
@@ -0,0 +1,28 @@
+/* let car = 
+{
+    make: 'bmw',
+    model: '745li',
+    year: '2012'
+}; */
+
+/* function Car(make, model, year)
+{
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+let myCar = new Car ('bmw', '745li', 2010);
+console.log(myCar); */
+
+function MyFunction()
+{
+    console.log('i am a simple function');
+}
+let myFunction = new MyFunction();
+console.log(typeof myFunction);
+
+
+
+
+
+
--- natives.js
+++ natives.js
@@ -1,4 +1,20 @@
-let pattern = /xyz/;
+/* let pattern = /xyz/;
 let value = 'THis a xyz test';
 console.log(pattern.test(value));
 console.log(value.replace(pattern, 'just'));
+ */
+// strings,numbers,boolen are primitive types (have built in,,,natives functions)
+/* 
+let myString = new String('howdy');
+console.log(myString);
+console.log(myString.toString());
+
+let myPrimitive = 'THIS IS CRAZY';
+console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLowerCase();
+console.log(typeof myPrimitive); */
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitive = myNumber.valueOf();
+console.log(typeof myPrimitive);
