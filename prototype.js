ff1240aa4b93462287e11d307faa81c57c400098
Neema_Mwende <gracepete12@gmail.com>
2023-09-16 19:30:19 +0300

    new

--- prototype.js
+++ prototype.js
@@ -21,31 +21,32 @@ console.log(Object.getPrototypeOf(newCar));
 
 originalCar.doors = 4;
 originalCar.color = 'red';
+newCar.make = 1800;
 console.log(newCar.doors);
 console.log(Object.getPrototypeOf(newCar));
+console.log(originalCar.hasOwnProperty('doors'));
+console.log(newCar.hasOwnProperty('doors'));
+console.log(newCar.hasOwnProperty('make'));
 
 // newCar.cc = 1800;
 // newCar.color = 'blue';
 // // console.log(Object.getPrototypeOf(originalCar));
-// console.log(Object.getPrototypeOf(newCar));
-console.log(newCar.cc);
+ // console.log(Object.getPrototypeOf(newCar));
+// console.log(newCar.cc);
 
 // to display the attributes of the original car
 for (let key in originalCar) {
     console.log(key + ": " + originalCar[key]);
 }
 
-
-newCar.cc = 1800;
-newCar.color = 'blue';
-
 // Creating a new object from an existing object
-let newCar = Object.create(originalCar);
-newCar.color = 'Blue';
+// let newCar = Object.create(originalCar);
+// newCar.color = 'Blue';
+// newCar.cc = 1800;
 
-for (let key in newCar) {
-    console.log(key + ": " + newCar[key]);
-}
-
+// for (let key in newCar) {
+//     console.log(key + ": " + newCar[key]);
+// }
+ 
 
 
