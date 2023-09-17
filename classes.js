8e3dcd5b20d370c780b1e0952045b3e207392ee1
Neema_Mwende <gracepete12@gmail.com>
2023-09-17 21:15:59 +0300

    extend keyword

--- classes.js
+++ classes.js
@@ -1,21 +1,47 @@
-//declaration
-class Car
-{
-    constructor(make, model, year)
-    {
+// Class name should start with an uppercase letter (Car instead of car)
+class Car {
+    constructor(make, model, year) {
         this.make = make;
         this.model = model;
         this.year = year;
     }
-    print()
-    {
-        console.log('${this.make} ${this.model} ${this.year}');
-        // console.log(`Make: ${this.make} Model:${this.model}, Year:${this.year}`);
+    print() {
+        // Use backticks (``) for template literals
+        console.log(`${this.make} ${this.model} (${this.year})`);
+        // You can also use the commented line below
+        // console.log(`Make: ${this.make} Model: ${this.model}, Year: ${this.year}`);
     }
 }
-let myCar = new Car ('bmw', '42232', 2102);
+
+// Correct the case of 'Car' when creating an instance 
+let myCar = new Car('BMW', '42232', 2022);
 myCar.print();
 
+class SportsCar extends Car
+{
+    revEngine()
+    {
+        console.log('reeving the engine for ' + this.model);
+    }
+}
+let mySportsCar = new SportsCar('dodge', 'viper', 2011);
+mySportsCar.print();
+mySportsCar.revEngine();
+// class newGift extends Car
+// {
 
-//expressionclear
-// let car = class {};
+// }
+// let newGiftCar = new newGift('audi', 'baby', 8923);
+// newGiftCar.print();
+
+
+
+
+
+
+
+
+
+
+
+
