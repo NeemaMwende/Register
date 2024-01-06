7bf4b5188ed89c8dc83efea71d4f78b2ea2c6829
JohnDoe <gracepete12@gmail.com>
2024-01-06 08:59:15 +0000

    fetch api

---
+++ async.js
@@ -0,0 +1,5 @@
+// a simple fetch API example
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(error => console.log(error));
