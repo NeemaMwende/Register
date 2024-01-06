97f48a05613c5de0e831b706043fa05ac5ba6e81
JohnDoe <gracepete12@gmail.com>
2024-01-06 09:11:54 +0000

    using await keyword inside the async function

--- async.js
+++ async.js
@@ -3,3 +3,21 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => console.log(json))
   .catch(error => console.log(error));
+
+  //Using Await in place of .then() method chaining
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+const json = await response.json();
+console.log(json)
+//Now if you run the code above, you might get an error like this:
+//SyntaxError: await is only valid in async functions and the top level bodies of modules
+//This error occurs because the await keyword must be used inside an asynchronous function or a module.
+
+//The Async Keyword
+//To create an asynchronous function, you need to add the async keyword before your function name.
+async function runProcess() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json = await response.json();
+    console.log(json)
+  }
+  
+  runProcess();
